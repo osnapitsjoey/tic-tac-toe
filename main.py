d9056970e430e9967b59393af7099a5f417666d1
Albert Smith <osnapitsjoey@gmail.com>
2025-06-23 11:27:00 -0400

    draw a nice board for the player. make legal move and place piece work

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
-BOARD_SIZE = 3
+BOARD_SIZE = 5
 
-
-def create_board(board_size):
+# Game Board
+def create_board(board_size) -> list:
 
     """
     Create a tic-tac-toe board with the specified size.
@@ -19,13 +19,43 @@ def create_board(board_size):
         row = []
         for col in range(board_size * 2 - 1): # doubles the size of grid to accommodate spacers, and stops one short of adding a third spacer
             if col % 2 == 0: # checks if dividing the current index of col is zero, if true places a '-', if false, adds a '|'
-                row.append("-") # playable space
+                row.append(" - ") # playable space
             else:
-                row.append("|") # spacer
+                row.append(" | ") # spacer
         board.append(row) # builds the row up until it finishes the for loops, starting the row list over again
 
     return board
 
+def draw_board(board) -> str:
+    """Displays the game board as characters aesthetically"""
+    string = ''
+    for row in board:
+        for char in row:
+            string += char
+        print(string)
+        string = ""
+        
 
-board_coords = create_board(BOARD_SIZE)
-print(board_coords)
+# Game Logic
+
+def legal_move(user_move, user_row, user_col, board):
+    pass 
+
+def place_piece(user_piece, user_row, user_col, board):
+    # if legal_move():
+    if user_col == 1:
+        board[user_row - 1][0] = " " + user_piece + " "
+    elif user_col % 2 == 0:
+        board[user_row - 1][user_col] = " " + user_piece + " "
+    else:
+        board[user_row - 1][user_col + 1] = " " + user_piece + " "
+    return board
+
+
+board_list = create_board(BOARD_SIZE)
+#draw_board(board_list)
+user_piece = 'X'
+user_row = 2
+user_col = 5
+place_piece(user_piece, user_row, user_col, board_list)
+draw_board(board_list)
